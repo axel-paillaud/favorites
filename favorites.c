295dec17b95928de105336d047166ec3ce316618
Shaun-maker <a.paillaud75@gmail.com>
2022-04-12 18:01:32 +0200

    Trying to add exit function

--- favorites.c
+++ favorites.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 //prototype
 char get_input();
+bool check_input();
 
 // global variable
 // 2100 car longueur max d'hyperlien IE est de 2083 caractères
@@ -12,7 +15,7 @@ int main(void)
 	do
 	{
 	get_input();
-	printf("%s\n", input);
+	check_input();
 	}
 	while (input != "exit");
 }
@@ -21,4 +24,22 @@ char get_input()
 {
 	printf("Pass the link: \n");
 	scanf("%2099s", input);
+}
+
+bool check_input()
+{
+	char *n = input;
+	char exit[4] = "exit"; 
+	int j = strlen(n);
+	char arr[j];
+	for (int i = 0; i <= j; i++)
+	{
+		arr[i] = n[i];
+	}
+	printf("%s\n", arr);
+	if ( arr == exit)
+	{
+		printf("Vous venez de taper exit\n");
+		return true;
+	}
 }
